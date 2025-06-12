475c3f8f06bfb4585796cbc25c8d795252cb0cec
yeasin-islam <info.yeasinislam@gmail.com>
2025-06-13 01:39:03 +0600

    Change upcomingmarathon card design

--- src/components/UpcomingMarathons.jsx
+++ src/components/UpcomingMarathons.jsx
@@ -14,20 +14,22 @@ const UpcomingMarathons = () => {
             <div className='container mx-auto my-6'>
                 <h2 className="text-3xl font-bold text-center mb-8">Upcoming Marathons</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {marathons.map(event => (
-                        <div key={event.id} className="card mx-4 lg:mx-0 bg-base-300 shadow-xl">
+                    {marathons.map(marathon => (
+                        <div key={marathon.id} className="card mx-4 md:mx-0 bg-base-300 shadow-xl">
                             <figure>
                                 <img
-                                    src={event.image}
-                                    alt={event.title}
+                                    src={marathon.image}
+                                    alt={marathon.title}
                                     className="h-48 w-full object-cover"
                                 />
                             </figure>
                             <div className="card-body">
-                                <h2 className="card-title">{event.title}</h2>
-                                <p>📍 {event.location}</p>
-                                <p>📅 {new Date(event.date).toLocaleDateString()}</p>
-                                <p>🏃 Distance: {event.distance}</p>
+
+                                <h2 className="text-xl font-bold text-center"> {marathon.title}</h2>
+                                <p className="text-gray-500 text-center">{marathon.description}</p>
+                                <p className='font-medium'>🏃 Distance: {marathon.distance}</p>
+                                <p className='font-medium'>📍 Location: {marathon.location}</p>
+                                <h4>📅 Marathon Date: <span className='text-[#09982F] font-semibold'>{new Date(marathon.date).toLocaleDateString()}</span></h4>
                             </div>
                         </div>
                     ))}
--- src/pages/Marathons.jsx
+++ src/pages/Marathons.jsx
@@ -28,7 +28,7 @@ const Marathons = () => {
 
     return (
         <section className='bg-base-200'>
-            <div className='fontStyle py-12 px-4 md:px-0 sm:px-6 lg:px-8 text-center container mx-auto'>
+            <div className='fontStyle py-12 px-4 md:px-0 text-center container mx-auto'>
                 {/* Section Tag & Description */}
                 <div className="mb-8">
                     <h2 className="text-3xl md:text-4xl font-bold mb-4">
